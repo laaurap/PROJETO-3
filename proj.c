a4d3766e3bfb3ad83e70a818d6b1d85f7fa99110
laaurap <146039258+laaurap@users.noreply.github.com>
2023-11-11 12:44:30 +0000

    ADICIONEI AS FUNCOES DE ATUALIZAR, DELETAR, SALVAR

--- proj.c
+++ proj.c
@@ -35,5 +35,126 @@ void listaTarefas(int tam, struct lista *lt){ // FUNÇÃO UTILIZADA PARA LISTAR 
         printf("DESCRICAO: %s\n",lt[i].descricao);
         printf("ESTADO: %s\n\n", status[lt[i].status]);
     }
-    
+
+void atualizaTarefa(int tam, struct lista *lista, int index) {
+    int prioridade_novo, estado_novo, resposta;
+
+    printf("\nQUAL CAMPO DESEJA ALTERAR? (Prioridade, categoria, descrição, estado) [1, 2, 3, 4, respectivamente]");
+    scanf("%d", &resposta);
+
+    switch(resposta) {
+    case 1:
+        printf("\nDIGITE A PRIORIDADE DA TAREFA (1-10) [novo]: ");
+        scanf("%d", &prioridade_novo);
+        lista[index - 1].prioridade = prioridade_novo;
+        break;
+    case 2:
+        printf("\nDIGITE A CATEGORIA DA TAREFA [novo]: ");
+        scanf(" %299[^\n]", lista[index - 1].categoria);
+        break;
+    case 3:
+        printf("\nDIGITE A DESCRICAO DA TAREFA [novo]: " );
+        scanf(" %99[^\n]", lista[index - 1].descricao);
+        break;
+    case 4:
+        printf("\nESTADO DA TAREFA (COMPLETO, EM ANDAMENTO, NÃO INICIADO) [0, 1, 2 respectivamente]: ");
+        scanf("%d", &estado_novo);
+
+         lista[index - 1].status = estado_novo;
+        break;
+    default:
+        break;
+        }
+    }
+
+int tamanho(struct lista *lt) { // FUNÇÃO QUE RETORNA O TAMANHO ATUAL DA LISTA DE TAREFAS
+    FILE *strArquivo = fopen("ListaDeTarefas", "rb");
+    int aux = 0;
+    if (strArquivo) {
+        // CONTA O NÚMERO DE TAREFAS (STRUCTS) PRESENTES NO ARQUIVO ATRAVÉS DA VARIÁVEL 'aux'
+        while (fread(&lt[aux], sizeof(struct lista), 1, strArquivo) == 1) {
+            aux++;
+        }
+        fclose(strArquivo);
+    }
+    else {
+        printf("ARQUIVO NAO ENCONTRADO.");
+    }
+    return aux;
+}
+
+void deletaTarefa(int *tam, struct lista *lt, int posTarefa) { // FUNÇÃO PARA DELETAR UMA TAREFA.
+    if (posTarefa < 1 || posTarefa > *tam) { // AQUI É ESCOLHIDA A POSIÇÃO DA TAREFA QUE O USUÁRIO DESEJA DELETAR NO ARRAY DA LISTA
+        printf("OPCAO INVALIDA. DIGITE UM NUMERODE 1 A %d.\n", *tam);
+        return;
+    }
+
+    for (int i = posTarefa - 1; i < *tam - 1; i++) { // SUBSTITUI AS INFORMAÇÕES DAS POSIÇÕES PELA POSIÇÃO SEGUINTE (MOVE AS OUTRAS TAREFAS PARA "TRÁS" PARA PREENCHER O "BURACO"
+        lt[i] = lt[i + 1];
+    }
+
+    (*tam)--; // DIMINUI O TAMANHO DA LISTA EM 1
+    printf("A TAREFA %d FOI DELETADA COM SUCESSO.\n", posTarefa);
+}
+
+void salvaTexto(int tam, struct lista *lista, int prioridade) {
+    char nome[300];
+
+    printf("Digite o nome do arquivo para salvar (insira '.txt' ao final do nome): ");
+    scanf("%s", nome);
+    FILE *arquivo = fopen(nome, "w");
+
+    fprintf(arquivo, "\n--- LISTA DE TAREFAS PRIORIDADE: %d", prioridade);
+    if(arquivo){
+        for(int i = 0; i < tam; i++){
+                if(prioridade==lista[i].prioridade){
+                    fprintf(arquivo, "\nPRIORIDADE: %d", lista[i].prioridade);
+                    fprintf(arquivo, "\nCATEGORIA: %s", lista[i].categoria);
+                    fprintf(arquivo, "\nESTADO: %s", status[lista[i].status]);
+                    fprintf(arquivo, "\nDESCRIÇÃO: %s", lista[i].descricao);
+                    fprintf(arquivo, "\n");
+                }
+        }
+
+        fclose(arquivo);
+    }
+
+}
+
+void salvaCategoriaPrioeidade(int tam, struct lista *lista, int prioridade, char categoria[300]){
+     char nome[300];
+
+    printf("Digite o nome do arquivo para salvar (insira '.txt' ao final do nome): ");
+    scanf("%s", nome);
+    FILE *arquivo = fopen(nome, "w");
+
+    fprintf(arquivo, "\n--- LISTA DE TAREFAS CATEGORIA E PRIORIDADE: %s\n", categoria);
+    if(arquivo){
+         qsort(lista, tam, sizeof(struct lista), compara_sort);
+
+         for(int i = 0; i < tam; i++){
+                if((prioridade==lista[i].prioridade) && (strcmp(categoria, lista[i].categoria) == 0)){
+                    fprintf(arquivo, "PRIORIDADE: %d\n",lista[i].prioridade);
+                    fprintf(arquivo, "CATEGORIA: %s\n",lista[i].categoria );
+                    fprintf(arquivo, "DESCRICAO: %s\n",lista[i].descricao);
+                    fprintf(arquivo, "ESTADO: %s\n\n", status[lista[i].status]);
+                    fprintf(arquivo, "\n");
+                }
+        }
+
+        fclose(arquivo);
+    }
+}
+
+
+void salvar(int tam, struct lista *lt) { // FUNÇÃO PARA ESCREVER A LISTA DE TAREFAS NO ARQUIVO.
+    FILE *strArquivo= fopen("ListaDeTarefas", "wb");;
+
+    if (strArquivo) {
+        // Escreve as tarefas no arquivo
+        fwrite(lt, sizeof(struct lista), tam, strArquivo);
+    }
+    fclose(strArquivo);
+}
+
 }
